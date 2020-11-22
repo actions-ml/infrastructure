8920c06953992e4bb81568f617982dfd4cc80a73
Sora Morimoto <sora@morimoto.io>
2020-11-22 22:48:39 +0900

    Tweak terraform/scaleway/ssh_keys.tf
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- terraform/scaleway/ssh_keys.tf
+++ terraform/scaleway/ssh_keys.tf
@@ -11,15 +11,17 @@ data "http" "github_keys" {
 }
 
 resource "scaleway_account_ssh_key" "ssh_keys" {
-  for_each = { for i in flatten([for username in local.github_usernames :
-    [for i, k in compact(split("\n", data.http.github_keys[username].body)) : {
-      name = username
-      public_key = element(
-        compact(split("\n", data.http.github_keys[username].body)),
-        i
-      )
-    }]
-  ]) : "${i.name}_${i.public_key}" => i }
+  for_each = { for idx, i in flatten([for username in local.github_usernames :
+    [for idx, _line in
+      compact(split("\n", data.http.github_keys[username].body)) :
+      {
+        name = username
+        public_key = element(
+          compact(split("\n", data.http.github_keys[username].body)),
+          idx
+        )
+      }
+  ]]) : format("%s.%d", i.name, idx) => i }
 
   name       = each.value.name
   public_key = each.value.public_key
