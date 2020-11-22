4ca166159e0e50b922e8c0b62ca4bc608a86d390
Sora Morimoto <sora@morimoto.io>
2020-11-23 01:57:54 +0900

    Add hashicorp/http as required provider
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- terraform/scaleway/versions.tf
+++ terraform/scaleway/versions.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = ">= 0.13"
 
   required_providers {
+    http = {
+      source  = "hashicorp/http"
+      version = "2.0.0"
+    }
+
     scaleway = {
       source  = "scaleway/scaleway"
       version = "1.17.2"
