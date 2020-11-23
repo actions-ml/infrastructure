dc250acc78767656e7f719e94cbb7e4f0ee91a70
Sora Morimoto <sora@morimoto.io>
2020-11-23 09:00:19 +0900

    Tweak terraform/scaleway/buckets.tf
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- terraform/scaleway/buckets.tf
+++ terraform/scaleway/buckets.tf
@@ -1,5 +1,15 @@
-resource "scaleway_object_bucket" "object_bucket" {
-  name   = "actions-ml-infrastructure"
-  acl    = "private"
+locals {
+  object_buckets = {
+    actions-ml-infrastructure = {
+      acl = "private"
+    }
+  }
+}
+
+resource "scaleway_object_bucket" "object_buckets" {
+  for_each = local.object_buckets
+
+  name   = each.key
+  acl    = each.value.acl
   region = "fr-par"
 }
